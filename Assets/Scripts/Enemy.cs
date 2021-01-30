8d61606bfd496bb718984b7d7580308eb013a4c1
James-1723 <james20031225@gmail.com>
2021-01-30 20:50:14 +0800

    Collision Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody rig;
+    public float thrust = 1.0f;
+    void Start()
+    {
+        this.rig = GetComponent<Rigidbody>();
+    }
+
+    void Update() 
+    {
+        if (Impulse.RightImpulse == true)
+        {
+            rig.AddForce(thrust, 0, 0, ForceMode.Impulse);
+            Impulse.RightImpulse = false;
+            
+        }
+        if (Impulse.LeftImpulse == true)
+        {
+            Debug.Log(33);
+            rig.AddForce(-thrust, 0, 0, ForceMode.Impulse);
+            Impulse.LeftImpulse = false;
+        }
+    } 
+}
