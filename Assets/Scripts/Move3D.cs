ddb3b96f35e3f0c7eec3b18980874df30fe0918d
James-1723 <james20031225@gmail.com>
2021-01-30 15:11:41 +0800

    Character Movement
    Right and Left movement have been done

---
+++ Assets/Scripts/Move3D.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public enum SIDE{ Left, Mid, Right }
+
+public class Move3D : MonoBehaviour
+{
+    public SIDE m_Side = SIDE.Mid;
+    float NewXPos = 0f;
+    public bool SwipeLeft;
+    public bool SwipeRight;
+    public float XValue;
+    private CharacterController m_char;
+
+    void Start()
+    {
+        transform.position = new Vector3(0,0,6);
+        m_char = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwipeLeft = Input.GetKeyDown(KeyCode.LeftArrow);
+        SwipeRight = Input.GetKeyDown(KeyCode.RightArrow);
+        Vector3 Movedir = new Vector3(1,0,0);
+        Vector3 mMovedir = new Vector3(-1,0,0);
+        if (SwipeLeft)
+        {
+            if (m_Side == SIDE.Mid)
+            {
+                m_Side = SIDE.Left;
+                m_char.Move(mMovedir * XValue);
+            }
+            else if (m_Side == SIDE.Right)
+            {
+                m_Side = SIDE.Mid;
+                m_char.Move(mMovedir * XValue);
+            }
+        }
+        if (SwipeRight)
+        {
+            if (m_Side == SIDE.Mid)
+            {
+                m_Side = SIDE.Right;
+                m_char.Move(Movedir * XValue);
+            }
+            else if (m_Side == SIDE.Left)
+            {
+                m_Side = SIDE.Mid;
+                m_char.Move(Movedir * XValue);
+            }
+        }
+    }
+}
+
