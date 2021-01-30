e3125b7a64962eeb94dde6555f685ca3558ec0c2
bogay <pojay11523@gmail.com>
2021-01-30 23:44:51 +0800

    Add opening scene (just for demo now)

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private float duration;
+    [SerializeField]
+    private bool autoDisable;
+
+    // do the pop up
+    public void Do()
+    {
+        this.target.SetActive(true);
+        var trans = this.target.transform;
+        trans.localScale = Vector3.zero;
+        trans
+            .DOScale(Vector3.one, this.duration)
+            .SetEase(Ease.OutCubic)
+            .OnComplete(() => this.target.SetActive(!this.autoDisable));
+    }
+}
